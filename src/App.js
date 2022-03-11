26bc563c77d3e9a2f7951b4c097a869c8c3ecc50
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-03-11 13:47:24 +0100

    Added component, styles and contents in the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './GlobalStyles';
+import Heading from './components/Heading';
+import PricingComponent from './components/PricingComponent';
+
+import styled from 'styled-components';
+import pattern from './images/bg-pattern.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<AppContainer>
+			<GlobalStyles />
+			<Heading />
+			<PricingComponent />
+		</AppContainer>
+	);
 }
 
 export default App;
+
+const AppContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+  justify-content: center;
+  align-items: center;
+	width: 100vw;
+	height: 100vh;
+	background: url(${pattern}) no-repeat top;
+`;
